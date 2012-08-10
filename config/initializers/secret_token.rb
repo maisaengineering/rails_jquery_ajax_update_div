e907dd955a989b1bf0f16052fdea13adbd0989e0
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-08-10 15:38:40 +0530

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJqueryAjaxUpdateDiv::Application.config.secret_token = '29a177d31cb688445567120ff8a275cb50efb4c33e82b5443edb3325e9ead1a82cc5b0d3cab2a06cebed85c101d899c285d2c682d175dfddb61d44c20e3422ea'
